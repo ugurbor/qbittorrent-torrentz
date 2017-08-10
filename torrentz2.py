9f8d89179671e0275ab7d4fc7bedf700204edfd6
Ugur Bor <ugurbohr@gmail.com>
2017-08-09 23:58:26 -0700

    Changed 10 page to only first page.

--- torrentz2.py
+++ torrentz2.py
@@ -23,27 +23,22 @@ class torrentz2(object):
         print(download_file(info))
 
     def search(self, what, cat='all'):
-        i = 0
-        while i<11:
-            res = 0
-            page = retrieve_url(self.url+'/search?f=%s&p=%d/'%(what, i))
-            p = re.compile('<a href="/(?P<link>[0-9a-f]{40})">(?P<name>.*?)</a>.*?'+
-                              '<span title="(\d+)">.*?(?P<size>\d+ MB)</span>.*?'+
-                              '<span>(?P<seeds>(\d+,)?\d+)</span>.*?'+
-                              '<span>(?P<leech>(\d+,)?\d+)</span>')
+        res = 0
+        page = retrieve_url(self.url+'/search?f=%s/'%(what))
+        p = re.compile('<a href="/(?P<link>[0-9a-f]{40})">(?P<name>.*?)</a>.*?'+
+                          '<span title="(\d+)">.*?(?P<size>\d+ MB)</span>.*?'+
+                          '<span>(?P<seeds>(\d+,)?\d+)</span>.*?'+
+                          '<span>(?P<leech>(\d+,)?\d+)</span>')
 
-            for match in p.finditer(page):
-                    m = p.search( match.group(0))
-                    if m:
-                            t = m.groupdict()
-                            t['name'] = re.sub('<.*?>', '', t['name'])
-                            t['desc_link'] = self.url + "/" + t['link']
-                            t['link'] = 'magnet:?xt=urn:btih:'+t['link']
-                            t['engine_url'] = self.url
-                            t['seeds'] = t['seeds'].replace(',', '')
-                            t['leech'] = t['leech'].replace(',', '')
-                            prettyPrinter(t)
-                            res = res + 1
-            if res == 0:
-                    break
-            i += 1
+        for match in p.finditer(page):
+                m = p.search( match.group(0))
+                if m:
+                        t = m.groupdict()
+                        t['name'] = re.sub('<.*?>', '', t['name'])
+                        t['desc_link'] = self.url + "/" + t['link']
+                        t['link'] = 'magnet:?xt=urn:btih:'+t['link']
+                        t['engine_url'] = self.url
+                        t['seeds'] = t['seeds'].replace(',', '')
+                        t['leech'] = t['leech'].replace(',', '')
+                        prettyPrinter(t)
+                        res = res + 1
