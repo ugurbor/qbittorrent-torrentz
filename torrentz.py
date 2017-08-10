b77a6b18a277cfebf4efe7bbce2876c9fd7d53ef
Ugur Bor <ugurbohr@gmail.com>
2017-08-09 23:10:11 -0700

    Changed URL to torrentz2.eu and updated regex to match new pattern.

--- torrentz.py
+++ torrentz.py
@@ -2,38 +2,47 @@
 #AUTHOR: Teemu Pajarinen (manakeri@enomeno.com)
 #Public Domain
 
-from novaprinter import prettyPrinter
-from helpers import retrieve_url, download_file
 import re
 
-class torrentz(object):
-	url = 'http://torrentz.eu'
-	name = 'torrentz'
-	supported_categories = {'all': 'all'}
+try:
+    from novaprinter import prettyPrinter
+    from helpers import retrieve_url, download_file
+except:
+    pass
 
-	def download_torrent(self, info):
-		print download_file(info)
+class torrentz2(object):
+    url = 'http://torrentz2.eu'
+    name = 'torrentz2'
+    supported_categories = {
+            'all': 0}
+    
+    def __init__(self):
 
-	def search(self, what, cat='all'):
-		i = 0
-		while i<11:
-			res = 0
-			page = retrieve_url(self.url+'/search?f=%s&p=%d/'%(what, i))
-                        p = re.compile('<a href="/(?P<link>[0-9a-f]{40})"><b>(?P<name>.*?)</a>.*?'+
-                                          '<span class="s">(?P<size>\d+ MB)</span>.*?'+
-                                          '<span class="u">(?P<seeds>\d+)</span>.*?'+
-                                          '<span class="d">(?P<leech>\d+)</span>')
+    def download_torrent(self, info):
+        print(download_file(info))
 
-                        for match in p.finditer(page):
-                                m = p.search( match.group(0))
-                                if m:
-                                        t = m.groupdict()
-                                        t['name'] = re.sub('<.*?>', '', t['name'])
-                                        t['desc_link'] = self.url + "/" + t['link']
-                                        t['link'] = 'magnet:?xt=urn:btih:'+t['link']
-                                        t['engine_url'] = self.url
-                                        prettyPrinter(t)
-                                        res = res + 1
-                        if res == 0:
-                                break
-			i += 1
+    def search(self, what, cat='all'):
+        i = 0
+        while i<11:
+            res = 0
+            page = retrieve_url(self.url+'/search?f=%s&p=%d/'%(what, i))
+            p = re.compile('<a href="/(?P<link>[0-9a-f]{40})">(?P<name>.*?)</a>.*?'+
+                              '<span title="(\d+)">.*?(?P<size>\d+ MB)</span>.*?'+
+                              '<span>(?P<seeds>(\d+,)?\d+)</span>.*?'+
+                              '<span>(?P<leech>(\d+,)?\d+)</span>')
+
+            for match in p.finditer(page):
+                    m = p.search( match.group(0))
+                    if m:
+                            t = m.groupdict()
+                            t['name'] = re.sub('<.*?>', '', t['name'])
+                            t['desc_link'] = self.url + "/" + t['link']
+                            t['link'] = 'magnet:?xt=urn:btih:'+t['link']
+                            t['engine_url'] = self.url
+                            t['seeds'] = t['seeds'].replace(',', '')
+                            t['leech'] = t['leech'].replace(',', '')
+                            prettyPrinter(t)
+                            res = res + 1
+            if res == 0:
+                    break
+            i += 1
